03442eaad2b28c6f73f355737f1710ff424c76d7
yjs01 <yjs01016@naver.com>
2022-05-24 19:36:53 +0900

    article 테이블 스키마 작성

--- routes/blog.js
+++ routes/blog.js
@@ -2,28 +2,12 @@ const express = require('express');
 const res = require('express/lib/response');
 const router = express.Router();
 
-/* 테스트 데이터 */
-// const articles = [
-//     {
-//         userId: 1,
-//         id: 1,
-//         title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-//         body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-//     },
-//     {
-//         userId: 1,
-//         id: 2,
-//         title: "qui est esse",
-//         body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-//     },
-// ];
-
 router.get('/article', (req, res) => {
     res.send('게시글 목록이 들어갈 페이지입니다.');
 });
 
-router.get("/article/:id", (req, res) => {
-    const id = req.params.id;
+router.get("/article/:articleId", (req, res) => {
+    const id = req.params.articleId;
     const [detail] = articles.filter(item => item.id === Number(id));
     res.json({ detail });
 })
