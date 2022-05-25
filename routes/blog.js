7c62dce66480b25fc9b586789ac894b5dfb7d85a
yjs01 <yjs01016@naver.com>
2022-05-25 13:07:05 +0900

    게시글 상세 조회 API 수정

--- routes/blog.js
+++ routes/blog.js
@@ -5,23 +5,21 @@ const router = express.Router();
 // 게시글 조회
 router.get('/article', async (req, res) => {
     const articles = await Article.find();
-
     res.json({ articles: articles });
 });
 
 // 게시글 작성
 router.post('/article', async (req, res) => {
     const { articleId, title, author, date, content, password } = req.body;
-    
     const createdArticle = await Article.create({ articleId, title, author, date, content, password });
     res.json({ article: createdArticle });
 
 })
 
 // 게시글 상세 조회
-router.get('/article/:articleId', (req, res) => {
+router.get('/article/:articleId', async (req, res) => {
     const id = req.params.articleId;
-    const [detail] = articles.filter(item => item.id === Number(id));
+    const detail = await Article.findOne({ articleId: id });
     res.json({ detail });
 })
 
