69f239eb8ee1d8eb5ffc449ccf17ad0a8db8edc7
yjs01 <yjs01016@naver.com>
2022-05-26 23:58:14 +0900

    게시글 수정 API 개선

--- routes/blog.js
+++ routes/blog.js
@@ -13,7 +13,6 @@ router.post('/article', async (req, res) => {
     const { articleId, title, author, date, content, password } = req.body;
     const createdArticle = await Article.create({ articleId, title, author, date, content, password });
     res.json({ article: createdArticle });
-
 })
 
 // 게시글 상세 조회
@@ -25,12 +24,13 @@ router.get('/article/:articleId', async (req, res) => {
 
 // 게시글 수정
 router.put('/article/:articleId', async (req, res) => {
-    const { articleId, title, content, password } = req.body;
+    const id = req.params.articleId;
+    const { title, content, password } = req.body;
 
-    const article = await Article.find({ articleId: Number(articleId) });
+    const article = await Article.find({ articleId: Number(id) });
     const pwd = article[0].password;
     if (article.length && (password === pwd)) {
-        await Article.updateOne({ articleId: Number(articleId) }, { $set: { title, content } });
+        await Article.updateOne({ articleId: Number(id) }, { $set: { title, content } });
     }
 
     res.json({ success: true });
@@ -38,12 +38,13 @@ router.put('/article/:articleId', async (req, res) => {
 
 // 게시글 삭제
 router.delete("/article/:articleId", async (req, res) => {
-    const { articleId, password } = req.body;
+    const id = req.params.articleId;
+    const { password } = req.body;
 
-    const article = await Article.find({ articleId: Number(articleId) });
+    const article = await Article.find({ articleId: Number(id) });
     const pwd = article[0].password;
     if (article.length && (password === pwd)) {
-        await Article.deleteOne({ articleId });
+        await Article.deleteOne({ articleId: Number(id) });
     }
   
     res.json({ result: "success" });
